6d2d22abb93d109e30994219681c670b92e9bf30
Bartosz <Bartosz>
2023-05-01 16:58:26 +0200

    first commit

---
+++ src/main/java/com/bard/spring5api/Spring5apiApplication.java
@@ -0,0 +1,13 @@
+package com.bard.spring5api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5apiApplication.class, args);
+	}
+
+}
